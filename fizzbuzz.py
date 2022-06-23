9104ec8f5a5c765b61abf419178f6f438f2a25a0
rabielf <rabielf@gmail.com>
2022-06-23 17:50:21 +0900

    feat : Print i if i is times of 3.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,13 @@
 # 3의 배수: fizz
 # 5의 배수: buzz
-# 15의 배수: fizzbuzz
+# 15의 배수: fizzbuzz done
 # 나머지: 숫자(1-45) done
 
 for i in range(1,45+1):
     if i%15==0:
         print('fizzbuzz')
-    #elif i%3==0:
-    #    print('fizz')
+    elif i%3==0:
+        print('fizz')
     #elif i%5==0:
     #    print('buzz')
     else:
